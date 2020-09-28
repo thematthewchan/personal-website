726e3c76f71033128d122b5b7fb23a17ec5e2311
Matt <xtremematt911@gmail.com>
2020-09-28 17:15:44 -0400

    Added Projects page

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+$('#example').tooltip(options)
+
 document.getElementById('date').innerHTML = new Date().toDateString();
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
 // Comment
 let name = 'Matt';
 console.log(name);
